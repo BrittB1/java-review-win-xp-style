12e2f4142b26df6868e1a20c7496e5a2fd9d2b07
Brittany Buchanan <157894600+BrittB1@users.noreply.github.com>
2026-08-27 22:20:54 -0400

    feat: added xp desktop w/ wallpaper and window

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
+import "./App.css"
+
 function App() {
+    return (
+        <>
+            <div className="desktop">
 
-  return (
-    <>
-<h1> Java OOP Review - Coming Soon</h1>
-    </>
-  )
+                <div className= "window" style={{width: 400}}>
+                    <div className="title-bar">
+                    <div className="title-bar-text">Java OOP Review</div>
+                    <div className="title-bar-controls">
+
+                    <button aria-label="Minimize"></button>
+                    <button aria-label="Maximize"></button>
+                    <button aria-label="Close"></button>
+                </div>
+            </div>
+            <div className="window-body">
+                <p>Welcome! Lessons coming soon..</p>
+            </div>
+            </div>
+            </div>
+        </>
+    )
 }
-
 export default App
